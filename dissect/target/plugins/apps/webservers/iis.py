0c425b68348b7b2982903a06a804f758694c01de
JSCU-CNI <121175071+JSCU-CNI@users.noreply.github.com>
2023-01-12 17:49:45 +0100

    add nginx, apache and caddy webserver log parsers

--- dissect/target/plugins/apps/webservers/iis.py
+++ dissect/target/plugins/apps/webservers/iis.py
@@ -1,18 +1,16 @@
 import re
+from datetime import datetime, timezone
 from functools import lru_cache
+from pathlib import Path
 from typing import Generator, List, Tuple
 
-from pathlib import Path
-from datetime import datetime, timezone
 from defusedxml import ElementTree
-
 from flow.record.base import RE_VALID_FIELD_NAME
 
 from dissect.target import plugin
+from dissect.target.exceptions import UnsupportedPluginError
 from dissect.target.helpers import fsutil
 from dissect.target.helpers.record import TargetRecordDescriptor
-from dissect.target.exceptions import UnsupportedPluginError
-
 
 LOG_RECORD_NAME = "filesystem/windows/iis/logs"
 
@@ -269,6 +267,9 @@ class IISLogsPlugin(plugin.Plugin):
             self.target.log.info("Parsing IIS log file %s in %s format", log_file, log_format)
             yield from parse_func(log_file)
 
+    @plugin.export(record=BasicRecordDescriptor)
+    def history(self) -> Generator[TargetRecordDescriptor, None, None]:
+        return self.logs()
 
 def replace_dash_with_none(data: dict) -> dict:
     """Replace "-" placeholder in dict values with None"""
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,7 +4,7 @@ from tempfile import TemporaryDirectory
 import pytest
 
 from dissect.target.helpers import config, keychain, docs
-from dissect.target.plugins.os.windows.iis import IISLogsPlugin
+from dissect.target.plugins.apps.webservers.iis import IISLogsPlugin
 
 from ._utils import absolute_path
 
@@ -78,7 +78,7 @@ def test_docs_plugin_functions_desc():
     assert functions_short_desc
     desc_lines = functions_short_desc.splitlines()
 
-    assert len(desc_lines) == 1
+    assert len(desc_lines) == 2
     assert "iis.logs" in functions_short_desc
     assert "Return contents of IIS (v7 and above) log files." in functions_short_desc
     assert "output: records" in functions_short_desc
